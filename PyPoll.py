bfaa7147d375c046bfbb0aa164e77f6f3564de67
snpanicker <70725117+snpanicker@users.noreply.github.com>
2021-02-14 19:21:49 -0800

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,43 @@
+# Data needed
+# 1. Total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. Total number of votes each candidate received
+# 4. Percentage of votes each candidate won
+# 5. The winner of the election based on popular vote
+
+# Algorithm
+# 1. Open the data file.
+# 2. Write down the names of all the candidates.
+# 3. Add a vote count for each candidate.
+# 4. Get the total votes for each candidate.
+# 5. Get the total votes cast for the election.
+
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+ 
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+    # # Print each row in the CSV file.
+    # for row in file_reader:
+    #     print(row)
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write three counties to the file.
+    txt_file.write("Counties in the Election\n")
+    txt_file.write("----------------------------\n")
+    txt_file.write("Arapahoe\nDenver\nJefferson")
+
